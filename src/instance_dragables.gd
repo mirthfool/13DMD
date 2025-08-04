4c2496d2c9eacbc2d25d210e2f14e66a9921055a
hamishwhat <81645133+mirthfool@users.noreply.github.com>
2025-08-04 12:46:55 +1200

    Improved drag and drop system

---
+++ src/instance_dragables.gd
@@ -0,0 +1,10 @@
+extends GridContainer
+
+var dragable := preload("res://Scenes/texture_rect.tscn")
+
+var max_col: int = 7
+var max_row: int = 7
+
+func _ready() -> void:
+	for i in range(max_col * max_row):
+		add_child(dragable.instantiate())
--- src/texture_rect.gd
+++ src/texture_rect.gd
@@ -1,10 +1,14 @@
 extends TextureRect
 
-func _get_drag_data(at_position: Vector2) -> Variant:
+var previous_texture
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	previous_texture = texture
 	var preview_texture: TextureRect = TextureRect.new()
 	preview_texture.texture = texture
 	preview_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	preview_texture.size = Vector2(70, 70)
+	preview_texture.position = -0.5 * preview_texture.size
 
 	var preview: Control = Control.new()
 	preview.add_child(preview_texture)
@@ -14,8 +18,15 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 
 	return preview_texture.texture
 
-func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	return data is Texture2D
 
-func _drop_data(at_position: Vector2, data: Variant) -> void:
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	texture = data
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_DRAG_END and not is_drag_successful():
+		texture = previous_texture
+	if what == NOTIFICATION_DRAG_END and is_drag_successful():
+		previous_texture = null
+
