bcb5fec62b4afafee0472a20803735d5e6a3fa66
hamishwhat <81645133+mirthfool@users.noreply.github.com>
2025-08-06 10:27:22 +1200

    Implemented dragging system

--- src/instance_dragables.gd
+++ src/instance_dragables.gd
@@ -1,9 +1,9 @@
 extends GridContainer
 
-var dragable := preload("res://Scenes/texture_rect.tscn")
+@onready var dragable := preload("res://Scenes/texture_rect.tscn")
 
-var max_col: int = 7
-var max_row: int = 7
+@onready var max_col: int = 7
+@onready var max_row: int = 7
 
 func _ready() -> void:
 	for i in range(max_col * max_row):
