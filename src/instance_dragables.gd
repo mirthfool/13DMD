88aadb96d6fcf66bcd371027d7bf64e0fed10452
hamishwhat <81645133+mirthfool@users.noreply.github.com>
2025-08-06 10:27:38 +1200

    Get valid textures within grid

--- src/instance_dragables.gd
+++ src/instance_dragables.gd
@@ -8,3 +8,14 @@ extends GridContainer
 func _ready() -> void:
 	for i in range(max_col * max_row):
 		add_child(dragable.instantiate())
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_select"):
+		get_active_textures()
+
+func get_active_textures() -> Array[TextureRect]:
+	var active_textures: Array[TextureRect]
+	for child:TextureRect in find_children("*", "TextureRect", false, false):
+		if child.texture != null:
+			active_textures.append(child)
+	return active_textures
